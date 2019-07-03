45b92d5e9e9a15d6f150a3fd260a3bdc7dde1778
Бабин Юрий Юрьевич <babin@pcr.int>
2019-07-03 18:40:46 +0300

    setup and ipynb instruction update

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
+# this import is a quick fix of warning 'install_requires"
+# https://stackoverflow.com/questions/8295644/pypi-userwarning-unknown-distribution-option-install-requires
+
 from distutils.core import setup
+import setuptools
 
 setup(
   name = 'crispr_cas9',
   packages = ['crispr_cas9'],   
-  version = '0.1',      
+  version = '0.3',      
   license='MIT',        
-  description = 'script for indels counting in deep sequencing data',   
+  description = 'script for indels counting in sequencing data',   
   author = 'Yuriy Babin',                  
   author_email = 'babin.yurii@gmail.com',      
   url = 'https://github.com/babinyurii/crispr_cas9', 
-  download_url = 'https://github.com/babinyurii/crispr_cas9/archive/v_0.1.tar.gz',
-  keywords = ['bioinformatics', 'SNP'],   
+  download_url = 'https://github.com/babinyurii/crispr_cas9/archive/v_0.3.tar.gz',
+  keywords = ['bioinformatics', 'indels', 'sequencing', 'NGS'],   
   install_requires=[            
-          'pandas',
-          'biopython',
-          'matplotlib',
-          'numpy',
-          'seaborn'
+          'pandas==0.23.0',
+          'biopython==1.73',
+          'matplotlib==2.2.2',
+          'numpy==1.11.3',
+          'seaborn==0.8.1'
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      
